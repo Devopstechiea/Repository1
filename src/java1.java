07475a80e995fb5b5709e42ee68ee6da7b59a6c3
sowkhya <soukysv@gmail.com>
2023-04-06 11:48:06 -0400

    3commit

--- src/java1.java
+++ src/java1.java
@@ -4,10 +4,11 @@ public class java1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        int num =5 ;
+       int num2 =6;
        String name = "myfirstjava";
        char letter = 's';
        double a = 2.22;
-        System.out.println(num  + " is the value. ");
+        System.out.println(num2  + " is the value. ");
        
 	}
 
